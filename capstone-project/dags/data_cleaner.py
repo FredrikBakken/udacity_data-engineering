e2bbf19add63b42f4df7a8341fd4efa25ef0e092
Fredrik Bakken <fredda10x@gmail.com>
2020-06-03 09:38:02 +0200

    Docker network updated to avoid random host definitions.

--- capstone-project/dags/data_cleaner.py
+++ capstone-project/dags/data_cleaner.py
@@ -14,26 +14,7 @@ from airflow.operators.python_operator import PythonOperator
 
 default_args = {
     'owner': 'Fredrik Bakken',
-    # 'depends_on_past': False,
-    'start_date': days_ago(1),
-    # 'email': ['email@email.com'],
-    # 'email_on_failure': False,
-    # 'email_on_retry': False,
-    # 'retries': 1,
-    # 'retry_delay': timedelta(minutes=5),
-    # 'queue': 'bash_queue',
-    # 'pool': 'backfill',
-    # 'priority_weight': 10,
-    # 'end_date': datetime(2016, 1, 1),
-    # 'wait_for_downstream': False,
-    # 'dag': dag,
-    # 'sla': timedelta(hours=2),
-    # 'execution_timeout': timedelta(seconds=300),
-    # 'on_failure_callback': some_function,
-    # 'on_success_callback': some_other_function,
-    # 'on_retry_callback': another_function,
-    # 'sla_miss_callback': yet_another_function,
-    # 'trigger_rule': 'all_success',
+    'start_date': days_ago(0),
 }
 
 dag = DAG(
